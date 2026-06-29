e01c3644d43e034214468d5396803a7a64918334
Chengh <3067429910@qq.com>
2026-06-29 23:52:06 +0800

    后端添加允许访问的域名

--- meiduoshop_api/meiduoshop_api/settings.py
+++ meiduoshop_api/meiduoshop_api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-ady!=7me(z@@=eox-*6qyp$fdh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.meiduo.site', 'localhost', '127.0.0.1']
 
 
 # Application definition
